0c1b4ca18d45ad574cf5e2ca70c2d8a0c10c0dac
Maik Schneider <mas@xima.de>
2024-03-05 13:09:26 +0100

    feat: rename workspace middleware

---
+++ Classes/Middleware/ForceWorkspace.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Xima\XimaTypo3Recordlist\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use ReflectionClass;
+use TYPO3\CMS\Backend\Routing\Route;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ForceWorkspace implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /**
+         * Exclude AJAX or sub requests
+         *
+         * @var Route $route
+         * @phpstan-ignore-next-line
+         */
+        $route = $request->getAttribute('route');
+        if (!$route->getOption('module') || $route->getOption('ajax')) {
+            return $handler->handle($request);
+        }
+
+        /**
+         * Extract valid class name of target controller
+         *
+         * @phpstan-ignore-next-line
+         */
+        $target = $request->getAttribute('target');
+        if (!$target || !is_string($target)) {
+            return $handler->handle($request);
+        }
+        $classParts = GeneralUtility::trimExplode('::', $target);
+        if (count($classParts) !== 2 || !class_exists($classParts[0])) {
+            return $handler->handle($request);
+        }
+
+        /** @var BackendUserAuthentication $beUser */
+        $beUser = $GLOBALS['BE_USER'];
+
+        $reflectionClass = new ReflectionClass($classParts[0]);
+        $workspaceId = $reflectionClass->hasConstant('WORKSPACE_ID') ? $reflectionClass->getConstant('WORKSPACE_ID') : 0;
+
+        if ($beUser->workspace !== $workspaceId && is_int($workspaceId)) {
+            $beUser->setWorkspace($workspaceId);
+        }
+
+        return $handler->handle($request);
+    }
+}
--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -3,7 +3,7 @@
 return [
     'backend' => [
         'xima-typo3-recordlist/workspace' => [
-            'target' => \Xima\XimaTypo3Recordlist\Middleware\WorkspaceSwitch::class,
+            'target' => \Xima\XimaTypo3Recordlist\Middleware\ForceWorkspace::class,
             'after' => [
                 'typo3/cms-backend/backend-routing',
             ],
