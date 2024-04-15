29bd0c19ece63f995a0a2f40f34413b7242afa10
Maik Schneider <mas@xima.de>
2024-04-15 13:12:31 +0200

    Revert "fix: set site identifier to use for access check in recordlist modules"
    This reverts commit 31d9f29ebbf6045595c88f588ffaf6b1074db2dd.

--- Classes/Controller/AbstractBackendController.php
+++ Classes/Controller/AbstractBackendController.php
@@ -15,9 +15,6 @@ use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
-use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
@@ -79,16 +76,8 @@ abstract class AbstractBackendController implements BackendControllerInterface
         // Get site
         $site = $request->getAttribute('site');
         if (!$site instanceof Site) {
-            try {
-                $siteIdentifier = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(
-                    'xima_typo3_recordlist',
-                    'siteIdentifier'
-                );
-            } catch (ExtensionConfigurationExtensionNotConfiguredException|ExtensionConfigurationPathDoesNotExistException $e) {
-                $siteIdentifier = 'default';
-            }
             $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
-            $site = $siteFinder->getSiteByIdentifier($siteIdentifier);
+            $site = current($siteFinder->getAllSites());
         }
         if (!$site instanceof Site) {
             throw new SiteNotFoundException('Could not determine which site configuration to use', 1688298643);
