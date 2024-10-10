f1290e13e8e1ff66ac35289838edf71123e42516
Maik Schneider <mas@xima.de>
2024-10-10 16:00:42 +0200

    feat: add Category partial with Middleware to workaround category default values

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -11,5 +11,14 @@ return [
                 'typo3/cms-backend/output-compression',
             ],
         ],
+        'xima-typo3-recordlist/category-tree-manipulation' => [
+            'target' => \Xima\XimaTypo3Recordlist\Middleware\CategoryTreeManipulation::class,
+            'after' => [
+                'typo3/cms-backend/backend-routing',
+            ],
+            'before' => [
+                'typo3/cms-backend/output-compression',
+            ],
+        ],
     ],
 ];
