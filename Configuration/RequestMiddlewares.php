8af09a9ce732ff07dad9e1341e32c1a30d745b09
Maik Schneider <mas@xima.de>
2025-02-11 17:57:47 +0100

    fix: remove ForceWorkspace middleware

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,15 +2,6 @@
 
 return [
     'backend' => [
-        'xima-typo3-recordlist/workspace' => [
-            'target' => \Xima\XimaTypo3Recordlist\Middleware\ForceWorkspace::class,
-            'after' => [
-                'typo3/cms-backend/backend-routing',
-            ],
-            'before' => [
-                'typo3/cms-backend/output-compression',
-            ],
-        ],
         'xima-typo3-recordlist/category-tree-manipulation' => [
             'target' => \Xima\XimaTypo3Recordlist\Middleware\CategoryTreeManipulation::class,
             'after' => [
