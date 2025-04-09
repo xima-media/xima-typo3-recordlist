de4f7c00a61cc53bd5808b4f7101b6309ac4e604
Maik Schneider <mas@xima.de>
2025-04-09 11:31:33 +0200

    feat: add fixture for new editor user who is restricted to the workspace

---
+++ Tests/Acceptance/Fixtures/sys_filemount.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_filemounts` (`uid`, `pid`, `tstamp`, `deleted`, `hidden`, `sorting`, `description`, `title`, `identifier`, `read_only`)
+VALUES
+	(1, 0, 1744190810, 0, 0, 256, '', 'Files', '1:/Files/', 0);
