28b4da1404bf86651d9a8b0bae0c096d89cad088
Maik Schneider <mas@xima.de>
2025-07-21 11:48:37 +0700

    fix: editorconfig

--- Tests/Acceptance/Fixtures/tt_content.sql
+++ Tests/Acceptance/Fixtures/tt_content.sql
@@ -1,3 +1,3 @@
 INSERT INTO tt_content (uid, pid, CType, header, bodytext, colPos, sys_language_uid, l18n_parent, pi_flexform) VALUES
-  (1, 5, 'news_pi1', 'News', '', 0, 0, 0, ''),
+	(1, 5, 'news_pi1', 'News', '', 0, 0, 0, ''),
 	(2, 5, 'news_pi1', 'Nachrichten', '', 0, 1, 1, '');
