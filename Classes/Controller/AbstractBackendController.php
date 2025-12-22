7acac8a90b547e9d758eefc1c9b1b141c1b616e6
Maik Schneider <maik.schneider@xima.de>
2025-12-22 16:10:45 +0100

    feat: add js for row selection

--- Classes/Controller/AbstractBackendController.php
+++ Classes/Controller/AbstractBackendController.php
@@ -137,6 +137,9 @@ abstract class AbstractBackendController extends ActionController implements Bac
         $this->pageRenderer->getJavaScriptRenderer()->addJavaScriptModuleInstruction(
             JavaScriptModuleInstruction::create('@xima/recordlist/recordlist-doc-new-record.js')
         );
+        $this->pageRenderer->getJavaScriptRenderer()->addJavaScriptModuleInstruction(
+            JavaScriptModuleInstruction::create('@xima/recordlist/recordlist-row-selection.js')
+        );
 
         $this->setLanguages();
 
