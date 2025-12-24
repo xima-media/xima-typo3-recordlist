e50371d91dbd0e11074ae8fbfb9b3c94bbd7ea1e
Maik Schneider <maik.schneider@xima.de>
2025-12-25 00:03:29 +0100

    feat: extend sql fixtures, create admin user via fixture

--- Tests/Acceptance/Fixtures/sys_filemount.sql
+++ Tests/Acceptance/Fixtures/sys_filemount.sql
@@ -1,3 +1,5 @@
 INSERT INTO `sys_filemounts` (`uid`, `pid`, `tstamp`, `deleted`, `hidden`, `sorting`, `description`, `title`, `identifier`, `read_only`)
 VALUES
-	(1, 0, 1744190810, 0, 0, 256, '', 'Files', '1:/Files/', 0);
+	(1, 0, 1744190810, 0, 0, 256, '', 'Files', '1:/Files/', 0),
+	(2, 0, 1744190811, 0, 0, 512, '', 'Images', '1:/Images/', 0),
+	(3, 0, 1744190812, 0, 0, 768, '', 'Documents', '1:/Documents/', 1);
