94e5f752c9524df32ffc98399dcc02866ef691aa
Maik Schneider <mas@xima.de>
2026-06-24 15:13:45 +0200

    feat: resolve filter items via FormDataCompiler (#102)

--- Classes/Controller/AbstractBackendController.php
+++ Classes/Controller/AbstractBackendController.php
@@ -1771,22 +1771,18 @@ abstract class AbstractBackendController extends ActionController implements Bac
 
             $config = $GLOBALS['TCA'][$this->getTableName()]['columns'][$columnName] ?? [];
             if ($column['filter']['partial'] === 'Select') {
-                if ($config['config']['foreign_table'] ?? '') {
-                    $foreignTable = $config['config']['foreign_table'];
-                    $foreignTableLabel = $GLOBALS['TCA'][$foreignTable]['ctrl']['label'] ?? 'uid';
-                    $qb = $this->connectionPool->getQueryBuilderForTable($foreignTable);
-                    $records = $qb->select('uid', $foreignTableLabel)
-                        ->from($foreignTable)
-                        ->executeQuery()
-                        ->fetchAllAssociative();
-                    foreach ($records as $record) {
-                        $column['filter']['items'][$record['uid']] = [
-                            'label' => $record[$foreignTableLabel],
-                            'value' => $record['uid'],
-                        ];
+                if (($config['config']['type'] ?? '') === 'select') {
+                    $column['filter']['items'] = $this->relationResolver->resolveSelectFilterItems(
+                        $this->getTableName(),
+                        $columnName,
+                        $this->getCurrentPid(),
+                        $this->request
+                    );
+                    if ($config['config']['foreign_table'] ?? '') {
+                        $foreignTable = $config['config']['foreign_table'];
+                        $column['filter']['iconIdentifier'] = $GLOBALS['TCA'][$foreignTable]['ctrl']['typeicon_classes']['default'] ?? '';
+                        $column['filter']['label'] = $this->getLanguageService()->sL($GLOBALS['TCA'][$foreignTable]['ctrl']['title'] ?? '');
                     }
-                    $column['filter']['iconIdentifier'] = $GLOBALS['TCA'][$foreignTable]['ctrl']['typeicon_classes']['default'] ?? '';
-                    $column['filter']['label'] = $this->getLanguageService()->sL($GLOBALS['TCA'][$foreignTable]['ctrl']['title'] ?? '');
                 }
             }
 
