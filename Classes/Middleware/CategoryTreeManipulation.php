f15d0015d82c9d982889432655072e1a7284cb46
Markus Hackel <mha@xima.de>
2026-07-22 09:58:38 +0200

    refactor: improve code style

--- Classes/Middleware/CategoryTreeManipulation.php
+++ Classes/Middleware/CategoryTreeManipulation.php
@@ -39,12 +39,12 @@ class CategoryTreeManipulation implements MiddlewareInterface
             $response = $handler->handle($request);
             $treeData = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
             foreach ($treeData as &$treeItem) {
-                if (!in_array((int)$treeItem['identifier'], $overrideValues, true)) {
+                if (in_array((int)$treeItem['identifier'], $overrideValues, true)) {
+                    $treeItem['checked'] = true;
+                } else {
                     // uncheck items that are not within $overrideValues (possibly set via TSconfig TCAdefaults for new records)
                     $treeItem['checked'] = false;
-                    continue;
                 }
-                $treeItem['checked'] = true;
             }
             return new JsonResponse($treeData);
         } catch (\Exception) {
